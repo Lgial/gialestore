2ec3d62e2964b3af0466aa65b8cf42100e28f4ce
unknown <leandrogiardelli@hotmail.com>
2022-09-21 21:15:03 -0300

    Sincronizar counter

--- src/Components/ItemDetail/ItemDetail.jsx
+++ src/Components/ItemDetail/ItemDetail.jsx
@@ -1,7 +1,15 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ItemCount from '../ItemCount/ItemCount'
+import { Link } from 'react-router-dom'
+
 
 const ItemDetail = ({ item }) => {
+
+    const [goCart, setGoCart] = useState(false);
+
+    const onAdd = (cantidad) => {
+        setGoCart(true);
+    }
 
     return (
         <div className="detail">
@@ -15,8 +23,11 @@ const ItemDetail = ({ item }) => {
                     Lista de venta skins en CS:GO
                 </p>
                 <h3>${item.precio}.-</h3>
-
-                <ItemCount stock={10} initial={1}/>
+                {
+                    goCart
+                    ? <Link to='/cart'>Terminar compra</Link>
+                    :<ItemCount stock={10} initial={1} onAdd={onAdd}/>
+                }
             </div>
         </div>
     );
